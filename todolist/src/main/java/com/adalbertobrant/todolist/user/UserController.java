c1204a9f7097e8c463ae71ee8ad68360d0ab6b5c
adalbertobrant <adalbertobrant@gmail.com>
2023-10-13 12:53:08 -0300

    adicionado o UserModel e UserController da aplicação, bem como feito o método POST

---
+++ todolist/src/main/java/com/adalbertobrant/todolist/user/UserController.java
@@ -0,0 +1,18 @@
+package com.adalbertobrant.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println("Nome : " + userModel.getName());
+        System.out.println("Username : " + userModel.getUsername());
+        System.out.println("Senha : " + userModel.getPassword());
+    }
+}
---
+++ todolist/src/main/java/com/adalbertobrant/todolist/user/UserModel.java
@@ -0,0 +1,20 @@
+package com.adalbertobrant.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
