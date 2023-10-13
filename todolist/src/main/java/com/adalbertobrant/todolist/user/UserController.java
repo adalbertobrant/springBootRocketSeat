4a7acac32101b8029ffff27306f7345496d2e86f
adalbertobrant <adalbertobrant@gmail.com>
2023-10-13 14:00:43 -0300

    retornando os dados para o usuário e salvando no banco mas usando o UserModel

--- todolist/src/main/java/com/adalbertobrant/todolist/user/UserController.java
+++ todolist/src/main/java/com/adalbertobrant/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.adalbertobrant.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println("Nome : " + userModel.getName());
-        System.out.println("Username : " + userModel.getUsername());
-        System.out.println("Senha : " + userModel.getPassword());
+    public UserModel create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null){
+            System.out.println("Usuário Já Existe!!!");
+            return null;
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
